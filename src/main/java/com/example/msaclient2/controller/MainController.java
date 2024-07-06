44a8f6dac0c3bac31bbf011b55c52b647150f76c
Yoon-Jemin <yjm1800@naver.com>
2024-07-07 01:12:21 +0900

    first commit

---
+++ src/main/java/com/example/msaclient2/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.msaclient2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/ms2/second")
+    public String mainP(){
+        return "main";
+    }
+}
---
+++ src/test/java/com/example/msaclient2/MsaClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaClient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
